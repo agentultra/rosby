e9a14c4b0700b8657e700e62c94db8d968418084
James King <james@agentultra.com>
2020-07-07 21:43:20 -0400

    Modify src/Rosby/Server.hs
    Created a newtype `SocketDispatcher` to give our loop a `MonadLogger`
    instance.

--- src/Rosby/Server.hs
+++ src/Rosby/Server.hs
@@ -10,6 +10,7 @@ import Control.Monad.Trans
 import qualified Data.ByteString as B
 import Data.Text (Text())
 import qualified Data.Text as T
+import qualified Data.Text.Encoding as T
 import Network.Socket
 import qualified Network.Socket.ByteString as S
 import System.IO
@@ -31,6 +32,15 @@ newtype Handler a = Handler { runHandler :: ReaderT Context (LoggingT IO) a }
     , MonadIO
     )
 
+newtype SocketDispatcher a = SocketDispatcher { runDispatcher :: LoggingT IO a }
+  deriving
+    ( Applicative
+    , Functor
+    , Monad
+    , MonadLogger
+    , MonadIO
+    )
+
 handler :: Socket -> Handler ()
 handler socket = do
   (Context configs) <- ask
@@ -39,6 +49,17 @@ handler socket = do
   unless (B.null input) $ do
     liftIO $ S.sendAll socket input
     handler socket
+
+type LogLine = (Loc, LogSource, LogLevel, LogStr)
+
+dispatcher :: Chan LogLine -> Context -> Socket -> SocketDispatcher ()
+dispatcher logChan ctx socket = do
+  liftIO $ forever $ runStdoutLoggingT $ do
+    $(logDebug) "ROSBY!!!!"
+    (conn, _peer) <- liftIO $ accept socket
+    liftIO $ void $ forkFinally (doHandle conn logChan ctx) (const $ gracefulClose conn 5000)
+  where
+    doHandle conn logChan = runChanLoggingT logChan . runReaderT (runHandler $ handler conn)
 
 type Port = String
 
@@ -61,12 +82,7 @@ start = withSocketsDo $ runStdoutLoggingT $ do
   let h = serverHost rosbyConfigs
   let p = serverPort rosbyConfigs
   addr <- liftIO $ resolve (T.unpack h) (show p)
-  liftIO $ bracket (open addr) close (loop logChan ctx)
-  where
-    loop logChan ctx s = forever $ do
-      (conn, _peer) <- accept s
-      void $ forkFinally (doHandle conn logChan ctx) (const $ gracefulClose conn 5000)
-    doHandle conn logChan = runChanLoggingT logChan . runReaderT (runHandler $ handler conn)
+  liftIO $ bracket (open addr) close (runStdoutLoggingT . runDispatcher . dispatcher logChan ctx)
 
 open :: AddrInfo -> IO Socket
 open addr = do
