680515b48b72163b3fc22bd872986496b78493f3
James King <james@agentultra.com>
2020-08-06 22:22:29 -0400

    Modify test/Spec.hs
    Added some golden tests for the serializer

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,4 +1,9 @@
+{-# LANGUAGE TemplateHaskellQuotes #-}
+
+import Control.Monad
+import qualified Data.ByteString.Char8 as B
 import Test.Hspec
+import Test.Hspec.Golden
 
 import Rosby.Protocol.Command
 import Rosby.Protocol.Serial
@@ -25,3 +30,16 @@ main = hspec $ do
     it "should have a valid str as the first element in an array" $ do
       let cmd = Array 1 [Str 3 "LSKDJFLSKDJFLSKDJFLKSJDLKFJ"]
       fromPrim cmd `shouldBe` Left "Unrecognized command"
+
+  describe "Rosby.Protocol.Serial" $ do
+    let goodExamples
+          = [ ("A", "$3\r\nFOO\r\n")
+            , ("B", "$6\r\nFOOBAR\r\n")
+            , ("C", ":123\r\n")
+            , ("D", ":123908023948098203948098347384787\r\n")
+            , ("E", "*4\r\n$3\r\nFOO\r\n$3\r\nFOO\r\n$3\r\nFOO\r\n$3\r\nFOO\r\n")
+            ]
+    forM_ goodExamples $ \(testid, example) -> do
+      it ("parses " ++ example) $ do
+        let output = show $ runParser $ B.pack example
+        defaultGolden ((show 'runParser) ++ testid) output
